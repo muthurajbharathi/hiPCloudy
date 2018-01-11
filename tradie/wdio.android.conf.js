de3688ab6870b68a06c25ec0a5f3bde7069bcf26
Jaspreet23 <jaskbamrah@gmail.com>
2018-01-11 17:42:33 +1100

    config change
    congif change

--- tradie/wdio.android.conf.js
+++ tradie/wdio.android.conf.js
@@ -1,12 +1,8 @@
 const merge = require('deepmerge');
 const baseConf = require('../shared/wdio.android.conf.js');
 const tradieConf = require('./wdio.tradie.conf.js');
-
-
 const baseTradie = merge(baseConf, tradieConf);
 exports.config = merge(baseTradie.config, {
-  username: 'jaspreetbamrah@hipagesgroup.com.au',
-  password: 'dvmsq8v7qtnb9knrtvfk6k84',
   specs: ['./specs/*.js'],
   suites: {
     sanity: [
@@ -14,12 +10,14 @@ exports.config = merge(baseTradie.config, {
       // './specs/lead_price_feedback/lead_price_feedback_spec.js',
       // '../shared/specs/message_centre/messaging_spec.js',
       // '../shared/specs/message_centre/attachment_spec.js',
-      // './specs/login/successful_login_spec.js',
-      './specs/login/unsuccessful_login_spec.js'
+      //'./specs/login/successful_login_spec.js'
+     // './specs/login/unsuccessful_login_spec.js'
+     './specs/invites_accept_spec.js'
+      
     ],
     login: ['./specs/login_system_spec.js'],
     messageCentre: ['../shared/specs/message_centre/attachment_spec.js',
-      '../shared/specs/message_centre/messaging_spec.js'],
+      '../shared/spçecs/message_centre/messaging_spec.js'],
     paywall: ['./specs/paywall_spec.js'],
     oldSanity: ['./specs/1_sanity_spec.js'],
     leadPriceFeedback: ['./specs/lead_price_feedback/lead_price_feedback_spec.js'],
@@ -27,36 +25,16 @@ exports.config = merge(baseTradie.config, {
   },
 
   capabilities: [{
-    app: `${__dirname}/tradie.apk`,
-    launchTimeout: 90000,
-    CommandTimeout: 600,
-    appPackage: 'com.hip.tradie.android',
-    appActivity: '.ui.MainActivity',
-    rotatable: true
+    app: `${__dirname}/tradie.apk`
+    // appPackage: 'com.hip.tradie.android',
+    // appActivity: '.ui.SplashActivity'
   }],
-  protocol: 'https',
-  host: 'device.pcloudy.com',
-  port: 443,
-  appname: 'tradie.apk',
-  oSversion: '6.0.1',
-  count: 1,
-  platform: '1',
+
   appPackage: 'com.hip.tradie.android',
   appActivity: '.ui.MainActivity',
-  onPrepare(config, capabilities) {
-    /*var AppiumpCloudy = require('./sample');
-    instance = new AppiumpCloudy();
-    
-    instance.appiumInterface(__dirname + '/configs/config-android.json');
-    //instance.appiumInterface(__dirname + '/wdio.conf.pcloudy.js');*/
-  },
+
   before(capabilities, specs) {
     const custComs = require('../shared/lib/custom_commands.js');
     custComs.addCustCommands('android', process.cwd());
-  },
-  onComplete() {
-   /* const AppiumpCloudy = require('./sampleTest');
-    instance = new AppiumpCloudy();
-    instance.releasePCloudy();*/
   }
 });
